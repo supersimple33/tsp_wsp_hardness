55f4d206d744da4d55844290ed9634f77cb3e243
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2026-03-22 18:21:26 -0400

    level wise updates

--- any_metric_wspd.py
+++ any_metric_wspd.py
@@ -7,7 +7,7 @@ type DistMatrix[N: int] = np.ndarray[tuple[N, N], np.dtype[np.floating]]
 type PointIndices = np.ndarray[tuple[int], np.dtype[np.integer]] # np.unsignedinteger
 
 @njit
-def _stream_raw_pairs[N: int](dist_matrix: DistMatrix[N], eps: float) -> Iterator[tuple[PointIndices, PointIndices]]:
+def _stream_raw_pairs[N: int](dist_matrix: DistMatrix[N], eps: float) -> Iterator[list[tuple[PointIndices, PointIndices]]]:
     """Optimized numba code for stating the WSPD generation"""
     assert 1 > eps > 0, "undefined behavior outside this range"
     n = dist_matrix.shape[0]
@@ -46,6 +46,7 @@ def _stream_raw_pairs[N: int](dist_matrix: DistMatrix[N], eps: float) -> Iterato
 
         lower_bound = (2/eps) * (2**i)
         upper_bound = (16/eps) * (2**i)
+        wspd_i: list[tuple[PointIndices, PointIndices]] = []
 
         for j in range(len(packing)):
             for k in range(j+1, len(packing)):
@@ -54,20 +55,32 @@ def _stream_raw_pairs[N: int](dist_matrix: DistMatrix[N], eps: float) -> Iterato
                     A: PointIndices = np.nonzero(nearest_site == x)[0]
                     B: PointIndices = np.nonzero(nearest_site == y)[0]
 
-                    yield A, B
+                    wspd_i.append((A, B))
+
+        yield wspd_i
 
 def gen_wspd[N: int](dist_matrix: DistMatrix[N], eps: float) -> list[tuple[PointIndices, PointIndices]]:
     """Generates a WSPD for the given distance matrix and separation factor epsilon.
     Based on the algorithm described in "Well-Separated Pairs Decomposition Revisited" by Har-Peled et al.
     """
-    seen_pairs: set[tuple[bytes, bytes]] = set()
+    global_seen: set[tuple[bytes, bytes]] = set()
     wspd: list[tuple[PointIndices, PointIndices]] = []
-    for A, B in _stream_raw_pairs(dist_matrix, eps):
-        bytes_A, bytes_B = A.tobytes(), B.tobytes()
-        pair_id = (bytes_A, bytes_B) if bytes_A < bytes_B else (bytes_B, bytes_A)
-        if pair_id not in seen_pairs:
-            seen_pairs.add(pair_id)
-            wspd.append((A, B))
+    for wspd_i in _stream_raw_pairs(dist_matrix, eps):
+        level_ids = []
+        for A, B in wspd_i:
+            A_bytes = A.tobytes()
+            B_bytes = B.tobytes()
+            level_ids.append((A_bytes, B_bytes) if A_bytes < B_bytes else (B_bytes, A_bytes))
+        
+        level_set = set(level_ids)
+        new_ids = level_set - global_seen
+
+        global_seen.update(new_ids)
+
+        for i, id in enumerate(level_ids):
+            if id in new_ids:
+                wspd.append(wspd_i[i])
+
     return wspd
             
 
