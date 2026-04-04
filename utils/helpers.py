7ce631c1706e5262c20727be418e29d9dee871b0
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2026-04-04 12:40:16 -0400

    added some new helper functions

---
+++ utils/helpers.py
@@ -0,0 +1,33 @@
+import numpy as np
+from numba import njit
+
+@njit(inline="always")
+def _euclidean(points: np.ndarray, u: int, v: int) -> float:
+    acc = (points[u, 1] - points[v, 1]) ** 2
+    for k in range(1, points.shape[1]):
+        dv = points[u, k] - points[v, k]
+        acc += dv * dv
+    return np.sqrt(acc)
+
+@njit(inline="always", cache=True)
+def calc_tour_len_euc(points: np.ndarray, tour: np.ndarray) -> float:
+    """Calculate the length of a tour given the points and the tour order. Done in just numpy"""
+    acc = _euclidean(points, tour[-1], tour[0])
+    for i in range(len(tour) - 1):
+        u = tour[i]
+        v = tour[i + 1]
+        acc += _euclidean(points, u, v)
+    return acc
+
+@njit(inline="always", cache=True)
+def valid_tour(tour: np.ndarray, n: int) -> bool:
+    """Check if a tour is valid (contains all nodes exactly once)"""
+    if tour.shape[0] != n:
+        return False
+    seen = np.zeros(n, dtype=np.bool_)
+    for i in range(n):
+        node = tour[i]
+        if node < 0 or node >= n or seen[node]:
+            return False
+        seen[node] = True
+    return True
